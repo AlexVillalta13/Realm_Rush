d30311c59ef0f53e1acf1993157e56e54f63dc4e
DESKTOP-G07ROO4\alex- <alexvf93@gmail.com>
2018-02-02 02:51:42 +0100

    121. Introducing Coroutines

--- Realm_Rush_source/Assets/Scripts/CubeEditor.cs
+++ Realm_Rush_source/Assets/Scripts/CubeEditor.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [Range(1f, 20f)]
     [SerializeField] float gridSize = 10f;
+
+    TextMesh textMesh;
+
     Vector3 snapPos;
+
+    private void Awake()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+    }
+
     void Update ()
     {
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
-	}
+
+        string labelText = (snapPos.x / gridSize) + "," + (snapPos.z / gridSize);
+        textMesh.text = labelText;
+
+        gameObject.name = labelText;
+    }
 }
